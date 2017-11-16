e5dff1d88acde5f3111cd17658a3d0cf06e5a9de
Amrit Sidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2017-11-16 11:52:00 +0000

    Admin form not working

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,24 +6,19 @@ class PostsController < ApplicationController
   end
 
   def show
-    @posts = Post.all
     redirect_to action: :index
   end
 
   def admin
     @posts = Post.all
-    @post = Post.new
   end
 
-  def new #New objects can be instantiated as either empty or pre-set with attributes but not yet saved (pass a hash with key names
-          #matching the associated table column names). In both instances, valid attribute keys are determined by the
-          #column names of the associated table – hence you can’t have attributes that aren’t part of the table columns.
+  def new
     @posts = Post.all
     @post = Post.new
   end
 
-  def create #Creates an object (or multiple objects) and saves it to the database, if validations pass.
-             #The resulting object is returned whether the object was saved successfully to the database or not.
+  def create
     @post = Post.new(post_params)
     if @post.save
       redirect_to posts_path
@@ -41,8 +36,7 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
-  def update #Updates the attributes of the model from the passed-in hash and saves the record, all wrapped in a transaction.
-             #If the object is invalid, the saving will fail and false will be returned. Also aliased as: update_attributes
+  def update
     @posts = Post.all
     if @post.update_attributes(post_params)
       redirect_to post_path(@post), notice:  "The evidence for #{@post.title} has been edited."
@@ -51,8 +45,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def destroy #The "destroy" method takes the passed id, fetches the model from the database using the "find" method,
-              #then calls destroy on that.
+  def destroy
      @post = Post.find(params[:id])
      @post.destroy
      redirect_to posts_path, notice:  "The evidence for #{@post.title} has been deleted."
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get '/posts/admin' => 'posts#admin'
   get '/posts/:id/user_edit' => 'posts#user_edit', as: :user_edit
 
+  post '/posts/:id/admin' => 'posts#admin'
 
   resources :posts  #Instead of declaring separate routes for your index, show, new, edit, create, update and destroy actions,
                     #a resourceful route declares them in a single line of code.
